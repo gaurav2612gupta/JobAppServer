e3ed71a7dd170e1a2d2d0159f98fe6e10377cb16
GauravGupta0007 <gaurav.gupta@forcepoint.com>
2024-06-15 14:30:13 +0530

    first commit

---
+++ src/main/java/com/example/jobApp/JobRestController.java
@@ -0,0 +1,21 @@
+package com.example.jobApp;
+
+import com.example.jobApp.service.JobService;
+import com.example.jobApp.model.JobPost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class JobRestController {
+
+    @Autowired
+    private JobService jobService;
+
+    @GetMapping("jobPosts")
+    public List<JobPost> getAllJobs() {
+        return jobService.getAllJobs();
+    }
+}
---
+++ src/main/java/com/example/jobApp/service/JobService.java
@@ -0,0 +1,24 @@
+package com.example.jobApp.service;
+
+import com.example.jobApp.repo.JobRepo;
+import com.example.jobApp.model.JobPost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost jobPost) {
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs() {
+        System.out.println("get all jobs job service");
+        return repo.getAllJobs();
+    }
+}
